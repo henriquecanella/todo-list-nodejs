adf0d2474279b7a51c3586acd0ba2b6a56d6acbc
Henrique Canella <henrique.mandelli@gmail.com>
2019-07-08 18:27:41 -0300

    update and delete projects added

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ server.use(express.json());
 
 const projects = [];
 
-//Listar todos projetos
-server.get('/projects', (req, res) => {
-  return res.json(projects);
-});
-
 //Criar projeto
 server.post('/projects', (req, res)=>{
   const { id } = req.body;  
@@ -21,5 +16,38 @@ server.post('/projects', (req, res)=>{
   return res.json(projects);
 
 });
+
+//Listar todos projetos
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+//Editar projeto com id passado por parametro
+server.put('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  for(i = 0; i < projects.length; i++){
+    if (id == projects[i].id){
+      projects[i].title = title;
+    }
+  }
+
+  return res.json(projects);
+});
+
+//Deletar o projeto com o id passado por parametro
+server.delete('/projects/:id', (req, res) => {
+  const { id } = req.params;
+
+  for (i = 0; i < projects.length; i++) {
+    if (id == projects[i].id) {
+      projects.splice(i, 1);
+    }
+  }
+
+  return res.send();
+});
+
 
 server.listen(3000);
