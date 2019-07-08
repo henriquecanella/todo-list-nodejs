b255807ff3e80a3067ea5c383ea4c09add4b6e0e
Henrique Canella <henrique.mandelli@gmail.com>
2019-07-08 17:54:26 -0300

    get all projects and create new project

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const server = express();
 
-server.get('/', (req, res) =>{
-  return res.send('hello world');
+server.use(express.json());
+
+const projects = [];
+
+//Listar todos projetos
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+//Criar projeto
+server.post('/projects', (req, res)=>{
+  const { id } = req.body;  
+  const { title } = req.body;
+  const tasks = [];  
+
+  projects.push({id, title, tasks});
+
+  return res.json(projects);
+
 });
 
 server.listen(3000);
